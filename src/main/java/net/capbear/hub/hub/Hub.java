e309cf063f287de08cc8beee974e1823562fc4fd
CrazyWillBear <crazywillbear@gmail.com>
2022-06-29 21:14:37 -0800

    Make players invisible

--- src/main/java/net/capbear/hub/hub/Hub.java
+++ src/main/java/net/capbear/hub/hub/Hub.java
@@ -23,6 +23,7 @@ public final class Hub extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.setJoinMessage("");
         Player player = event.getPlayer();
+        player.hidePlayer(player);
 
         TextComponent survival = new TextComponent("§3- §oSurvival"/*replace this with what you want the text to say*/);
         survival.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Join Survival server!"/*replace this with what you want the text to say when its hovered on*/).create()));

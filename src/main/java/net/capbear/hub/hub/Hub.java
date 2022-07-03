57a3bad1f781c14df3d24f057e96eb18d22893ae
CrazyWillBear <crazywillbear@gmail.com>
2022-07-02 19:32:55 -0800

    Disable leave messages

--- src/main/java/net/capbear/hub/hub/Hub.java
+++ src/main/java/net/capbear/hub/hub/Hub.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hub extends JavaPlugin implements Listener {
@@ -45,6 +46,11 @@ public final class Hub extends JavaPlugin implements Listener {
         player.sendMessage("§3§lSelect a server to join by clicking on it in chat!");
         player.sendMessage(survival);
         player.sendMessage(creative);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage("");
     }
 
     @Override
